9e6303afc614d479d43220815eb3ada000c3c531
Matias Perez <matiasperezpc@hotmail.com>
2025-01-16 11:25:14 -0300

    fix?

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -1,3 +1,9 @@
+export async function loader() {
+  return {
+    message: "Hello World",
+  };
+}
+
 export default function About() {
   return <div className="flex h-screen items-center justify-center">About Page</div>;
 }
